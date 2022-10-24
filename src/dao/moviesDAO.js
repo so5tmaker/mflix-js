c8376f9c65b2ee0ea0fc3b771fb6bf9247c5b05b
Viktor Sivulskiy <info@softmaker.kz>
2022-10-24 19:03:03 +0600

    Ticket Handling Errors

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -344,6 +344,16 @@ export default class MoviesDAO {
 
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
+      // here's how the InvalidId error is identified and handled
+      if (
+        e
+          .toString()
+          .startsWith(
+            "Error: Argument passed in must be a single String of 12 bytes or a string of 24 hex characters",
+          )
+      ) {
+        return null
+      }
       console.error(`Something went wrong in getMovieByID: ${e}`)
       throw e
     }
