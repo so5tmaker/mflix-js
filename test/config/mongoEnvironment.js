760bf2e049d286bf13a3f55725ad375d8e4d1566
Viktor Sivulskiy <info@softmaker.kz>
2022-10-24 18:10:21 +0600

    Ticket Connection Pooling

--- test/config/mongoEnvironment.js
+++ test/config/mongoEnvironment.js
@@ -7,9 +7,12 @@ module.exports = class MongoEnvironment extends NodeEnvironment {
         process.env.MFLIX_DB_URI,
         // TODO: Connection Pooling
         // Set the connection pool size to 50 for the testing environment.
+        {
+          poolSize: 50,
+        },
         // TODO: Timeouts
         // Set the write timeout limit to 2500 milliseconds for the testing environment.
-        { useNewUrlParser: true }
+        { useNewUrlParser: true },
       )
       await super.setup()
     }
